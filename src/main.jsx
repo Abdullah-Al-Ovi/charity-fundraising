5391df1dc6366054c3ce450cd040dae0a5b171e4
Noob-Ovi <abdullah.ovi098@gmail.com>
2025-01-20 02:16:10 +0600

    Added project structure

---
+++ src/main.jsx
@@ -0,0 +1,16 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import './index.css'
+import Routes from './Routes/Routes.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={Routes}>
+     
+    </RouterProvider>
+  </StrictMode>,
+)
